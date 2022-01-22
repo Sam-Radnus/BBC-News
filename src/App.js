4d12149a4bee8032ffae561742bedc64abe1a1bd
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-01-22 23:53:06 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -7,17 +7,39 @@ import AroundTheBBC from './Components/AroundTheBBC';
 import MostRead from './Components/MostRead';
 import Sport from './Components/Sport';
 import FindUs from './Components/FindUs';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import News from './Components/News';
 export default class App extends Component {
+  pageSize = 10;
+  apiKey = 'a70c1839ba1c41c5b29461feee78b4af';
   render() {
     return <div>
-      <TopNav/>
-      <Navbar/>
-      <Headlines/>
-      <MustSee/>
-      <MostRead/>
-      <AroundTheBBC/>
-      <Sport/>
-      <FindUs/>
+      <TopNav />
+      <Router>
+        <Navbar />
+        <Headlines />
+           <MustSee />
+           <MostRead />
+           <AroundTheBBC />
+           <Sport />
+           <FindUs />
+        <Routes>
+        
+        
+        <Route exact path="/Home">
+          
+          </Route> 
+        <Route exact path="/Business" element={<News key="Business" apiKey={this.apiKey} pageSize={this.pageSize} category="business"></News>}></Route>
+        <Route exact path="/Entertainment" element={<News key="Entertainment" apiKey={this.apiKey} pageSize={this.pageSize} category="entertainment"></News>}></Route>
+        <Route exact path="/General" element={<News key="General" apiKey={this.apiKey} pageSize={this.pageSize} category="general"></News>}></Route>
+        <Route exact path="/Health" element={<News key="Health" apiKey={this.apiKey} pageSize={this.pageSize} category="health"></News>}></Route>
+        <Route exact path="/Science" element={<News key="Science" apiKey={this.apiKey} pageSize={this.pageSize} category="science"></News>}></Route>
+        <Route exact path="/Sports" element={<News key="Sports" apiKey={this.apiKey} pageSize={this.pageSize} category="sports"></News>}></Route>
+        <Route exact path="/Technology" element={<News key="Technology" apiKey={this.apiKey} pageSize={this.pageSize} category="technology"></News>}></Route>
+        </Routes>
+   
+      </Router>
     </div>;
   }
 }
---
+++ src/Components/News.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import NewsItems from './NewsItems';
+export default class extends Component {
+    articles = []
+
+    static propTypes = {
+        pageSize: this.pageSize,
+        category: this.category
+    }
+    constructor() {
+        super()
+        this.state = {
+            articles: [],
+            apiKey: 'a70c1839ba1c41c5b29461feee78b4af'
+        }
+    }
+    async componentDidMount() {
+        const url = `https://newsapi.org/v2/top-headlines?country=gb&category=${this.props.category}&apiKey=a70c1839ba1c41c5b29461feee78b4af`;
+        let data = await fetch(url);
+        console.log(data);
+        let parseData = await data.json();
+        console.log(parseData);
+        this.setState({
+            articles: parseData.articles,
+            totalResults: parseData.totalResults
+        })
+    }
+    render() {
+
+        return <div className="container">
+            <div className="row">
+                {this.state.articles.map((element) => {
+                    return <div className="col-md-4" key={element.url}>
+                        <h1>{element.category}</h1>
+                         <NewsItems title={element.title} description={element.description} 
+                                 imageURL={element.urlToImage?element.urlToImage:"https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"} 
+                                 author={element.author?element.author:"BBC News "} date={element.publishedAt} newsURL={element.url}/> 
+                    </div>
+                })}
+            </div>
+        </div>;
+    }
+}
